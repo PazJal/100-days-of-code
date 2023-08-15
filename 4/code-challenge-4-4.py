3d87cd6254db90d9b981703d8db98abde6292234
PazJal <30651575+PazJal@users.noreply.github.com>
2023-08-15 12:14:53 +0300

    feat: add day 4 ex 4

---
+++ 4/code-challenge-4-4.py
@@ -0,0 +1,57 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+
+# Match results
+match_results = ["draw", "win" , "lose"]
+
+# Drawing mapping
+drawings = [rock, paper, scissors]
+
+# Prompt player
+msg = "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."
+print(msg)
+
+# Take player choice
+player_input = input()
+
+# Convert to int
+player_choice = int(player_input)
+print(drawings[player_choice])
+
+# Generate computer choice
+computer_choice = random.randint(0,2)
+print("Computer chose:")
+print(drawings[computer_choice])
+
+# Calculate match outcome:
+match_outcome = (player_choice - computer_choice) % 3
+
+# Print reuls
+print(f"You {match_results[match_outcome]}")
