bf86c5cf3e05db7e4214ef9737a1be1370b01321
PazJal <30651575+PazJal@users.noreply.github.com>
2023-08-21 10:17:17 +0300

    feat: add step 3 solution

--- 7/hangman.py
+++ 7/hangman.py
@@ -1,25 +1,27 @@
-import random
+#Step 3
 
+import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+#Create blanks
 display = []
-for letter in chosen_word:
-  display.append('_')
+for _ in range(word_length):
+    display += "_"
 
-guess = input("Guess a letter: ").lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while '_' in display:
+  guess = input("Guess a letter: ").lower()
 
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for index,letter in enumerate(chosen_word):
-    if letter == guess:
-        display[index] = letter
-
-print(display)
-
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+  #Check guessed letter
+  for position in range(word_length):
+      letter = chosen_word[position]
+      print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+      if letter == guess:
+          display[position] = letter
+  print(display)
+print("You won!")
