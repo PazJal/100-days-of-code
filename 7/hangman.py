6f5a311672ecd44eeea659c44b94a881a971d08c
PazJal <30651575+PazJal@users.noreply.github.com>
2023-08-21 10:12:35 +0300

    feat: add day7 step 2 solution

--- 7/hangman.py
+++ 7/hangman.py
@@ -1,16 +1,25 @@
-#Step 1 
-
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
 for letter in chosen_word:
-  if guess == letter:
-    print("Right")
-  else:
-    print("Wrong")
+  display.append('_')
+
+guess = input("Guess a letter: ").lower()
+
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for index,letter in enumerate(chosen_word):
+    if letter == guess:
+        display[index] = letter
+
+print(display)
+
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
