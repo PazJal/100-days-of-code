688a46051996ef51e2a3940f1f5ad54316dc45b5
PazJal <30651575+PazJal@users.noreply.github.com>
2023-08-21 21:00:56 +0300

    fix: remove completed todos

--- 7/hangman.py
+++ 7/hangman.py
@@ -4,14 +4,12 @@ import random
 from hangman_words import word_list
 from hangman_art import logo, stages
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 print(logo)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -24,8 +22,6 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -37,7 +33,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         lives -= 1
         print(f"{guess} is not in the word.")
         if lives == 0:
@@ -52,5 +47,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stages[lives])
