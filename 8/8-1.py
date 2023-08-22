3c5a2f03401f39fb03a4166768c7aefebe429060
PazJal <30651575+PazJal@users.noreply.github.com>
2023-08-22 10:10:07 +0300

    feat: add 8-1 sol

---
+++ 8/8-1.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+from math import ceil;
+
+def paint_calc(height, width, cover):
+  area = height * width
+  paint_cans = ceil(area / cover);
+  print(f"You'll need {paint_cans} cans of paint.")
+  return paint_cans
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
