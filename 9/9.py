a5482fda377dd58bda4f8510aee1165f38cd6911
PazJal <30651575+PazJal@users.noreply.github.com>
2023-08-23 22:54:17 +0300

    feat: finisehd day9 first chapter

---
+++ 9/9.py
@@ -0,0 +1,17 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+  "Loop": "The action of doing something over and over again.",
+}
+
+# Accessing a dictionary
+print(programming_dictionary["Function"])
+
+# Adding new items to dictionary.
+programming_dictionary["Touch Grass"] = "The futile act of trying to retreive some sanity."
+
+print(programming_dictionary)
+
+# Iterating through dictionary goes through the keys:
+for key in programming_dictionary:
+  print(key)
