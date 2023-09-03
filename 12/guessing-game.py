708ab3f92ef57417a4ad107268945761469cfc57
PazJal <30651575+PazJal@users.noreply.github.com>
2023-09-03 13:02:56 +0300

    feat: add task breakdown

---
+++ 12/guessing-game.py
@@ -0,0 +1,13 @@
+# TODO: Welcome prompt
+print("Welcome to the guessing game!")
+# TODO: Choose Difficulty
+def choose_difficulty():
+  user_input = input("Select a difficulty. Type 'easy' or 'hard': ")
+  return user_input
+# TODO: Prompt attmpets remaining
+def print_attempts_remaining(attempts):
+  print(f"You have {attempts} attempts remaining.")
+# TODO: Guess
+
+# TODO: Guess Feedback & update attempts
+# TODO: Win lose conditions.
