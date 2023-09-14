c9e4164946ef3224d97ec5146549d510ea42b328
PazJal <30651575+PazJal@users.noreply.github.com>
2023-09-14 14:32:39 +0300

    feat: add function for user money input

--- 15/15.py
+++ 15/15.py
@@ -32,22 +32,31 @@ resources = {
 
 money = 0
 
-#a function to print report
+# a function to print report
 def print_report():
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
     print(f"Money: ${money}")
 
-
+# Get the conins input and return the money amount. 
+def input_money():
+    quarters = int(input("how many quarters?: "))
+    dimes = int(input("how many quarters?: "))
+    nickles = int(input("how many quarters?: "))
+    pennies = int(input("how many quarters?: "))
+    
+    return (quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01)
+    
 
 
 print("Hello, Please order your coffee.")
 print_report()
 
+print(input_money())
+
 # TODO: Create general order prompt
 
-# TODO: Create money prompt
 
 # TODO: Create a function to test the resources
 
