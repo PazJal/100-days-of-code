3fa46250545b24429d06bee56475e1c69f2a1492
pj <pj@pj.com>
2024-09-06 22:08:31 +0300

    feat: 16 - 112 2nd challenge complete

--- 16/main.py
+++ 16/main.py
@@ -1,9 +1,18 @@
-from turtle import Turtle, Screen
-my_turt = Turtle()
-my_turt.shape("turtle")
-my_turt.color("cyan")
-my_turt.forward(100)
+#from turtle import Turtle, Screen
+#my_turt = Turtle()
+#my_turt.shape("turtle")
+#my_turt.color("cyan")
+#my_turt.forward(100)
 
-my_screen = Screen()
-my_screen.exitonclick()
+#my_screen = Screen()
+#my_screen.exitonclick()
 
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
