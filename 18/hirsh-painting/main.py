033eeba6f54918f1230bd35544ece807aa4382ae
PazJal <30651575+PazJal@users.noreply.github.com>
2024-12-04 10:52:04 +0200

    feat: fixes
    move to preset set of colors.
    fix error with new line dot placement
    hide turtule cursor

--- 18/hirsh-painting/main.py
+++ 18/hirsh-painting/main.py
@@ -6,19 +6,32 @@ import colorgram
 s = Screen()
 t = Turtle()
 DOTS_WIDTH  = 10
-DOTS_HEIGHT = 15
+DOTS_HEIGHT = 10
 
 s.colormode(255)
 t.speed(0)
 t.pensize(5)
-t.shape()
 t.penup()
 t.setposition(-200,200)
+t.hideturtle()
 
-colors = colorgram.extract(".\\18\\hirsh-painting\\image.jpg", 16)
+#colors = colorgram.extract(".\\18\\hirsh-painting\\image.jpg", 32)
+#rgb_colors = []
+#for item in colors:
+#    (r, g, b) = item.rgb
+#    rgb_colors.append((r,g,b))
+#print(rgb_colors)
+
+rgb_colors = [
+        (203, 163, 126), (235, 242, 247), (162, 167, 34), (235, 68, 127), (149, 51, 98), (236, 78, 53), 
+        (12, 144, 67), (238, 218, 78), (226, 115, 161), (9, 140, 181), (128, 31, 82), (62, 196, 223), 
+        (24, 174, 136), (167, 58, 50), (250, 231, 0), (25, 192, 214), (115, 187, 151), (241, 162, 186), 
+        (148, 221, 194), (238, 175, 158), (1, 116, 22), (126, 219, 232), (79, 16, 72), (128, 40, 38)
+        ]
+
 for j in range(0,DOTS_HEIGHT):
     for i in range(0,DOTS_WIDTH):
-        t.dot(20,choice(colors).rgb)
+        t.dot(20,choice(rgb_colors))
         t.penup()
         t.forward(40)
         t.pendown()
@@ -26,7 +39,6 @@ for j in range(0,DOTS_HEIGHT):
     t.setheading(270)
     t.forward(40)
     t.setheading(0 if j % 2 == 1 else 180)
-
-print(colors[0])
+    t.forward(40)
 
 s.exitonclick()
